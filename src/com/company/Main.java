10b9cb0346087c7401b0cee6f3e69d22b7769fea
sumsky108 <sumendrakadek108@gmail.com>
2019-08-22 13:28:12 +0700

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	int a=3600,b;
+	Scanner scan = new Scanner (System.in);
+	System.out.println("luas tabung 1:" + a);
+
+	b=(6*a);
+        System.out.println("luas tabung 6:" + b);
+    }
+}
+
